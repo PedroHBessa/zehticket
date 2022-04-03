ee53fa4095d1b0fd60c43ab15b3d28aed6f52d90
Pedro Bessa <pedro.hbessa@live.com>
2022-04-03 16:44:28 -0300

    add urls

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,49 @@
 import React, { useState } from "react";
 import "../styles/login.scss";
+import axios from "axios"
+import { useNavigate } from "react-router-dom"
+
+
 
 
 
 
 function Login(props) {
+
+
+  const location = useNavigate()
+
   const [user, setUser] = useState(null);
 
+
+  const [data, setData] = useState({
+    email: 'sharanga@xxx.com', 
+    password: '123456'
+  })
+
+  const [token, setToken] = useState(null)
+
   const [loginError, setLoginError] = useState("");
+
+
+
+
+ 
+
+
+const req = ()=> {
+
+  axios.get('https://api.zehticket.com.br/sanctum/csrf-cookie').then(response => {
+    axios.post("https://api.zehticket.com.br/api/login", data)
+  .then(response => {
+    localStorage.setItem('token',response.data.token)
+    location("/", {replace: true})
+
+  });
+});
+  
+
+}
 
   const closeWindow = () => {
     console.log((document.querySelector(".login-form").style.opacity = "0"));
@@ -18,30 +54,34 @@ function Login(props) {
     document.querySelector(".login-form").style.transform = "scale(1)";
   }, 50);
 
+
+
  
     
   return (
     <div>
       <section>
-        <form className="login-form">
+        <div className="login-form">
           <div className="close-btn" onClick={closeWindow}>
            icon
           </div>
           <h1>Welcome</h1>
 
           <label>
-            <input name="email" type="text" placeholder="email" />
+            <input name="email" type="text" placeholder="email"  /* onChange={setEmail(this)} *//>
           </label>
 
           <label>
-            <input name="password" type="password" placeholder="password" />
+            <input name="password" type="password" placeholder="password" /* onChange={setPassword(this)} *//>
           </label>
           <div className="login-form__error">{loginError}</div>
 
-          <button className="login-form__button">
+          <button onClick={()=>{req()}} className="login-form__button">
             <strong>Login</strong>
           </button>
-        </form>
+
+          <p>{token}</p>
+        </div>
       </section>
       
     </div>
